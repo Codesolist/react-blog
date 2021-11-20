e005cc0cbf22e86c6326d17934407358fcf72d14
Codesolist <alperen@biseo.com.tr>
2021-11-20 13:19:52 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { usePosition } from 'use-position';
+import './style.css';
 
 export default function App() {
+  const { latitude, longitude, speed, timestamp, accuracy, heading, error } =
+    usePosition();
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>{latitude}</h1>
+      <h1>{longitude}</h1>
     </div>
   );
 }
